5690c3bc1dfedf05379b77726001737dec243c9a
MandiriTraining <111072208+MandiriTraining@users.noreply.github.com>
2022-08-15 14:16:41 +0700

    3rd changes

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 from flask import Flask,render_template, request
-from flask_mysqldb import MySQL
+import mysql.connector
+  
+dataBase = mysql.connector.connect(
+  host ="188.166.221.246",
+  user ="training",
+  passwd ="training",
+  database = "employees"
+)
  
-app = Flask(__name__)
+# preparing a cursor object
+cursorObject = dataBase.cursor()
+  
+query = "select * from employees"
+cursorObject.execute(query)
+   
+myresult = cursorObject.fetchall()
+   
+for x in myresult:
+    print(x)
  
-app.config['MYSQL_HOST'] = '188.166.221.246'
-app.config['MYSQL_USER'] = 'training'
-app.config['MYSQL_PASSWORD'] = 'training'
-app.config['MYSQL_DB'] = 'employees'
- 
-mysql = MySQL(app)
- 
-@app.route('/form')
-def form():
-    return render_template('form.html')
- 
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if request.method == 'GET':
-        return "Login via the login Form"
-     
-    if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
-        mysql.connection.commit()
-        cursor.close()
-        return f"Done!!"
- 
-app.run(host='localhost', port=5000)
+# disconnecting from server
+dataBase.close()
